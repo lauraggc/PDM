e19f3222d8b234447accfa44f9feeb322e44d11b
lauraggc <laura.gonzalez@iteso.mx>
2023-09-10 13:02:45 -0600

    tarea3p

--- tarea3/android/app/src/main/kotlin/com/example/tarea3/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tarea3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ tarea3p/android/app/src/main/kotlin/com/example/tarea3p/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tarea3p
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
