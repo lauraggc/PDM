645c3777954511ded5dd3815d31088eb71028aa9
lauraggc <laura.gonzalez@iteso.mx>
2023-09-24 20:34:18 -0600

    changes

--- tarea3_5/android/app/src/main/kotlin/com/example/tarea3_5/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tarea3_5
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ tarea3_5p/android/app/src/main/kotlin/com/example/tarea3_5p/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tarea3_5p
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
